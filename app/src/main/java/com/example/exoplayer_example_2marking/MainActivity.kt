9c7f68eeba6f33a5ed19d303179be9295846ab4c
이교원 <cjswoakzld@naver.com>
2020-06-11 14:35:29 +0900

    Completed Sample ExoPlayer and change deprecated Method

--- app/src/main/java/com/example/exoplayer_example_2marking/MainActivity.kt
+++ app/src/main/java/com/example/exoplayer_example_2marking/MainActivity.kt
@@ -3,19 +3,25 @@ package com.example.exoplayer_example_2marking
 import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.exoplayer2.ExoPlaybackException
 import com.google.android.exoplayer2.ExoPlayerFactory.newSimpleInstance
+import com.google.android.exoplayer2.Player
 import com.google.android.exoplayer2.SimpleExoPlayer
-import com.google.android.exoplayer2.source.ExtractorMediaSource
+import com.google.android.exoplayer2.Timeline
 import com.google.android.exoplayer2.source.MediaSource
+import com.google.android.exoplayer2.source.ProgressiveMediaSource
+import com.google.android.exoplayer2.source.TrackGroupArray
 import com.google.android.exoplayer2.source.hls.HlsMediaSource
-import com.google.android.exoplayer2.ui.PlayerView
+import com.google.android.exoplayer2.trackselection.TrackSelectionArray
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
 import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory
+import com.google.android.exoplayer2.util.Log
 import com.google.android.exoplayer2.util.Util
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
-    private val exoPlayerView: PlayerView? = null
+//    private val exoPlayerView: PlayerView? = null
     private var player: SimpleExoPlayer? = null
 
     var playWhenReady:Boolean = true
@@ -39,13 +45,51 @@ class MainActivity : AppCompatActivity() {
 
     fun initializePlayer(){
         if (player == null){
-            player = newSimpleInstance(this.applicationContext)
-            exoPlayerView?.player = player
+            player = newSimpleInstance(
+                this.applicationContext
+            )
+
+//            player!!.addListener(object : Player.EventListener {
+//                fun onTimelineChanged(
+//                    timeline: Timeline?,
+//                    manifest: Any?
+//                ) {
+//                    Log.e("onTimelineChanged","call")
+//                }
+//
+//                override fun onTracksChanged(
+//                    trackGroups: TrackGroupArray,
+//                    trackSelections: TrackSelectionArray
+//                ) {
+//                    Log.e("onTracksChanged","call")
+//                }
+//
+//                override fun onLoadingChanged(isLoading: Boolean) {
+//                    Log.e("onLoadingChanged","call")
+//                }
+//                override fun onPlayerStateChanged(
+//                    playWhenReady: Boolean,
+//                    playbackState: Int
+//                ) {
+//                    Log.e("onPlayerStateChanged","call")
+//                }
+//
+//                override fun onPlayerError(error: ExoPlaybackException) {
+//                    //Catch here, but app still crash on some errors!
+//                    Log.e("onPlayerError","call")
+//                }
+//
+//                fun onPositionDiscontinuity() {}
+//            })
+            testExoPlayerView?.player = player
         }
 
         val sampleURL = "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
 
         val mediaSource:MediaSource = this!!.buildMediaSource(Uri.parse(sampleURL))!!
+        Log.e("mediaSource",mediaSource.toString())
+        Log.e("mediaSource",Uri.parse(sampleURL).toString())
+
 
         player?.prepare(mediaSource, true, false)
 
@@ -55,14 +99,19 @@ class MainActivity : AppCompatActivity() {
     private fun buildMediaSource(uri: Uri): MediaSource? {
         val userAgent = Util.getUserAgent(this, "blackJin")
         return if (uri.lastPathSegment!!.contains("mp3") || uri.lastPathSegment!!.contains("mp4")) {
-            ExtractorMediaSource.Factory(DefaultHttpDataSourceFactory(userAgent))
+            Log.e("if case","first")
+            ProgressiveMediaSource.Factory(DefaultHttpDataSourceFactory(userAgent))
                 .createMediaSource(uri)
         } else if (uri.lastPathSegment!!.contains("m3u8")) {
             //com.google.android.exoplayer:exoplayer-hls 확장 라이브러리를 빌드 해야 합니다.
+            Log.e("if case","second")
+
             HlsMediaSource.Factory(DefaultHttpDataSourceFactory(userAgent))
                 .createMediaSource(uri)
         } else {
-            ExtractorMediaSource.Factory(DefaultDataSourceFactory(this, userAgent))
+            Log.e("if case","else")
+
+            ProgressiveMediaSource.Factory(DefaultDataSourceFactory(this, userAgent))
                 .createMediaSource(uri)
         }
     }
@@ -73,7 +122,7 @@ class MainActivity : AppCompatActivity() {
             currentWindow = it.currentWindowIndex
             playWhenReady = it.playWhenReady
 
-            exoPlayerView?.player = null
+            testExoPlayerView?.player = null
             it.release()
             player = null
         }
