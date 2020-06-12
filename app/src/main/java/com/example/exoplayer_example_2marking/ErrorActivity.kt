d0f3abf9b37055d964df5fddeceffa0f28b61502
이교원 <cjswoakzld@naver.com>
2020-06-12 22:09:53 +0900

    Setting android gradle file

---
+++ app/src/main/java/com/example/exoplayer_example_2marking/ErrorActivity.kt
@@ -0,0 +1,20 @@
+//package com.example.exoplayer_example_2marking
+//
+//import android.content.Intent
+//import android.os.Bundle
+//import androidx.appcompat.app.AppCompatActivity
+//
+//class ErrorActivity : AppCompatActivity() {
+//    private lateinit var viewBinding: ErrorViewBinding
+//
+//    private val lastActivityIntent by lazy { intent.getParcelableExtra<Intent>(EXTRA_INTENT)}
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        viewBinding = DataBindingUtil.setContentView()
+//    }
+//
+//    companion object {
+//        const val EXTRA_INTENT = "EXTRA_INTENT"
+//    }
+//}
